72e48da4a796fb3ae1c56e48640342907d084d1a
Gime Cz <mgczarnievicz@gmail.com>
2025-05-29 18:50:07 +0200

    Add and Update Db

---
+++ src/db/models/Post.js
@@ -0,0 +1,14 @@
+// db/models/Joke.js
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+	title: { type: String, required: true },
+	url: { type: String, required: true },
+	description: { type: String, required: true },
+});
+
+const Post = mongoose.models.Post || mongoose.model('Post', postSchema);
+
+export default Post;
