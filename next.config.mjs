19ce3ecf40431cf6e52e6a751a0e2b71da1264de
Gime Cz <mgczarnievicz@gmail.com>
2025-05-29 18:47:17 +0200

    Add image url

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'images.unsplash.com',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'plus.unsplash.com',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'res.cloudinary.com',
+				port: '',
+				// pathname: "/dhckhirkm/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
